d50b4557bd4491587c5de7448d6bb9b7b20e4077
venkatesh kongara <vkongara048@gmail.com>
2025-07-11 09:06:50 +0000

    in head salistileAI icon should be there

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: 'SalistleAI | Early Access',
   description: 'Request early access to our AI-powered storybook generation tool.',
+  icons: {
+    icon: 'https://res.cloudinary.com/dsukslmgr/image/upload/v1752064727/ChatGPT_Image_Jul_9_2025_12_14_27_PM_l3fkk1.png',
+  }
 };
 
 export default function RootLayout({
